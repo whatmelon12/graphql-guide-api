1e8d87e0c0188ae7c680b5cd64335e228ccc9a2a
Omar Mejia <omar.mejia@admios-sa.com>
2022-12-22 10:04:15 -0500

    Add user auth0 github and schema

--- src/resolvers/User.js
+++ src/resolvers/User.js
@@ -5,6 +5,15 @@ export default {
         me: (_, __, context) => context.user
     },
 
+    User: {
+        id: ({ _id }) => _id,
+        photo(user) {
+            const githubId = user.authId.split('|')[1]
+            return `https://avatars.githubusercontent.com/u/${githubId}`
+        },
+        createdAt: user => user._id.getTimestamp()
+    },
+
     Mutation: {
         createUser(_, { user, secretKey }, { dataSources }) {
             if (secretKey !== process.env.SECRET_KEY) {
