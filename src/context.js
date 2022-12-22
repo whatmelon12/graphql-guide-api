5533982caf7940c43edce5b24024215ac96a349b
Omar Mejia <omar.mejia@admios-sa.com>
2022-12-22 09:46:11 -0500

    wip: auth users

--- src/context.js
+++ src/context.js
@@ -1,3 +1,4 @@
+import { db } from "./db";
 import { getAuthIdFromJWT } from "./util/auth";
 
 const API_KEYS = ['alohomora'];
@@ -5,18 +6,25 @@ const API_KEYS = ['alohomora'];
 export default async ({ req }) => {
     const context = {};
 
-    if (API_KEYS.includes(req.headers.authorization)) {
-        context.apiUser = true
-    } else {
-        const jwt = req.headers.authorization;
-        const authId = await getAuthIdFromJWT(jwt);
-        // console.log(authId);
-        if (authId === 'github|26941056') {
-            context.user = {
-                firstName: 'Omar',
-                lastName: 'Mejia'
-            }
-        }
+    // if (API_KEYS.includes(req.headers.authorization)) {
+    //     context.apiUser = true
+    // } else {
+    //     const jwt = req.headers.authorization;
+    //     const authId = await getAuthIdFromJWT(jwt);
+    //     // console.log(authId);
+    //     if (authId === 'github|26941056') {
+    //         context.user = {
+    //             firstName: 'Omar',
+    //             lastName: 'Mejia'
+    //         }
+    //     }
+    // }
+
+    const jwt = req.headers.authorization;
+    const authId = await getAuthIdFromJWT(jwt);
+    const user = await db.collection('users').findOne({ authId })
+    if (user) {
+        context.user = user
     }
 
     return context;
